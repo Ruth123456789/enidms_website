86e0ac32e439d1ef2a8bee1d44879e7c4d4e3313
mer29 <meronterefe123@gmail.com>
2020-11-23 18:18:30 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from "mdbreact";
+import FormPage from './component/form'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <br />
+      <FormPage />
+      
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/component/form.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from "mdbreact";
+import NavbarPage from './navbar'
+import { MDBDatePickerV5 } from 'mdbreact';
+import { Route, Router } from "react-router-dom/cjs/react-router-dom.min";
+import { ReportPage } from "./report";
+
+const FormPage = () => {
+  return (
+    <>
+
+      <br />
+      <NavbarPage />
+      <br />
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="6">
+            <form>
+              <p className="h4 text-center mb-4"> Add Educational Report</p>
+              
+              <label htmlFor="defaultFormContactNameEx" className="grey-text">
+                School Name
+              </label>
+              <input
+                type="text"
+                id="defaultFormContactNameEx"
+                className="form-control"
+              />
+
+              <br />
+              <label htmlFor="defaultFormContactEmailEx" className="grey-text">
+                Educational Title
+              </label>
+              <input
+                type="email"
+                id="defaultFormContactEmailEx"
+                className="form-control"
+              />
+              <br />
+
+              <label
+                htmlFor="defaultFormContactSubjectEx"
+                className="grey-text"
+              >
+                Date of Submission
+              </label>
+              <textarea
+                type="text"
+                id="defaultFormContactMessageEx"
+                className="form-control"
+                rows="3"
+              />
+              
+              <div className="text-center mt-4">
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <span
+                      className="input-group-text"
+                      id="inputGroupFileAddon01"
+                    >
+                      Upload
+                    </span>
+                  </div>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      className="custom-file-input"
+                      id="inputGroupFile01"
+                      aria-describedby="inputGroupFileAddon01"
+                    />
+                    <label
+                      className="custom-file-label"
+                      htmlFor="inputGroupFile01"
+                    >
+                      Choose file
+                    </label>
+                  </div>
+                </div>
+
+                <MDBBtn color="warning" outline type="submit">
+                  Submit
+                  <MDBIcon far icon="paper-plane" className="ml-2" />
+                </MDBBtn>
+              </div>
+            </form>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+    </>
+  );
+};
+
+export default FormPage;
